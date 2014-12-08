257bdb748e77ceabc88e81c371f27c3a0f12f815
Brian J. Burg <burg@cs.washington.edu>
2014-12-08 14:44:04 -0800

    Split up the demo file into libraries.

---
+++ demo/lib/EditorView.js
@@ -0,0 +1,118 @@
+var EditorView = function(model) {
+    this.model = model;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("shader-editor");
+
+    var item = null;
+
+    this.optionsElement = document.createElement("span");
+        this.optionsElement.classList.add("editor-options");
+        var optionsList = this.optionsElement.appendChild(document.createElement("ul"));
+        this.selectorElement = document.createElement("select");
+            this.selectorElement.id = "editor-selector";
+            var option = null;
+                option = document.createElement("option");
+                option.text = "Vertex Shader";
+                option.value = GLSL.Shader.Type.Vertex;
+            this.selectorElement.add(option, null);
+
+            option = document.createElement("option");
+                option.text = "Fragment Shader";
+                option.value = GLSL.Shader.Type.Fragment;
+            this.selectorElement.add(option, null);
+            item = optionsList.appendChild(document.createElement("li"));
+        item.appendChild(this.selectorElement);
+        var checkboxLabel = document.createElement("label");
+            this.emitDebuggerCheckbox = document.createElement("input");
+                this.emitDebuggerCheckbox.type = "checkbox";
+            checkboxLabel.textContent = "Pause Inside Shader";
+            checkboxLabel.appendChild(this.emitDebuggerCheckbox);
+            item = optionsList.appendChild(document.createElement("li"));
+        item.appendChild(checkboxLabel);
+    this.element.appendChild(this.optionsElement);
+
+    // Select the fragment shader by default.
+    this.selectorElement.options[1].selected = true;
+    this.selectorElement.addEventListener("change", this._shaderSelectorChanged.bind(this));
+
+    var editorStub = document.createElement("div");
+        editorStub.id = "shader-editor-stub";
+    this.element.appendChild(editorStub);
+
+    // Set up CodeMirror to replace the editor stub.
+    this._cm = CodeMirror(function(editorNode) {
+        editorStub.parentNode.replaceChild(editorNode, editorStub);
+        editorNode.id = editorStub.id;
+        editorNode.classList.add("stub");
+    }, {
+        value: "",
+        mode: "glsl",
+        lineNumbers: true
+    });
+
+    this._cm.on("changes", this._shaderEditorContentChanged.bind(this));
+    this.emitDebuggerCheckbox.addEventListener("change", this._emitDebuggerCheckboxChanged.bind(this));
+};
+
+EditorView.Event = {
+    ShaderTypeChanged: "editor-view-shader-type-changed"
+};
+
+EditorView.prototype = {
+    constructor: EditorView,
+    __proto__: GLSL.Object.prototype,
+
+    // Public
+
+    get activeShaderType()
+    {
+        return this.selectorElement.options[this.selectorElement.selectedIndex].value;
+    },
+
+    set activeShaderType(value)
+    {
+        var options = this.selectorElement.options;
+        for (var i = 0; i < options.length; ++i)
+            if (value === options[i].value)
+                options[i].selected = true;
+
+        this.refresh();
+    },
+
+    refresh: function()
+    {
+        this.autosizeHeight();
+
+        var selectedShader = this.model.shaderWithType(this.activeShaderType);
+        this._cm.setValue(selectedShader ? selectedShader.sourceText : "");
+    },
+
+    autosizeHeight: function()
+    {
+        var allowedHeight = this.element.offsetHeight - 5 + "px";
+        document.getElementById("shader-editor-stub").style.height = allowedHeight;
+    },
+
+    // Private
+
+    _shaderSelectorChanged: function(event)
+    {
+        console.assert(event.target === this.selectorElement, event.target);
+        console.assert(event.target.selectedIndex != -1, event.target);
+
+        this.refresh();
+        this.dispatchEventToListeners(EditorView.Event.ShaderTypeChanged, this.activeShaderType);
+    },
+
+    _shaderEditorContentChanged: function(event)
+    {
+        this.model.updateShaderWithType(this.activeShaderType, this._cm.getValue());
+    },
+
+    _emitDebuggerCheckboxChanged: function(event)
+    {
+        var activeShader = this.model.shaderWithType(this.activeShaderType);
+        activeShader.shouldEmitDebuggerStatement = !!this.emitDebuggerCheckbox.checked;
+    },
+};
---
+++ demo/lib/VariableView.js
@@ -0,0 +1,212 @@
+var VariableView = function(variable, suggestedValue) {
+    this.variable = variable;
+
+    this._fields = [];
+
+    var variableRow = this.element = document.createElement("div");
+    variableRow.classList.add("variable-row", variable.usage);
+
+    var usageLabel = variableRow.appendChild(document.createElement("span"));
+    usageLabel.classList.add("variable-usage", variable.usage);
+    usageLabel.textContent = variable.usage;
+
+    var qualifierLabel = variableRow.appendChild(document.createElement("span"));
+    qualifierLabel.classList.add("variable-qualifier", variable.qualifier);
+    qualifierLabel.textContent = variable.qualifier;
+
+    var label = variableRow.appendChild(document.createElement("span"));
+    label.classList.add("variable-label");
+    if (variable.builtin)
+        label.classList.add("builtin");
+    label.textContent = variable.name;
+
+    var typeLabel = variableRow.appendChild(document.createElement("span"));
+    typeLabel.classList.add(variable.type, "variable-type");
+    typeLabel.textContent = variable.type;
+
+    // TODO: handle sampler2D and samplerCube seperately, by adding a
+    // file upload widget and thumbnail preview.
+
+    var cellTable = variableRow.appendChild(document.createElement("table"));
+    cellTable.classList.add("variable-cells");
+
+    var rowCount = 1;
+    var colCount = 1;
+
+    switch (variable.type) {
+    case "float": break;
+    case "vec2":  colCount = 2; break;
+    case "vec3":  colCount = 3; break;
+    case "vec4":  colCount = 4; break;
+    case "mat2":  colCount = 2; rowCount = 2; break;
+    case "mat3":  colCount = 3; rowCount = 3; break;
+    case "mat4":  colCount = 4; rowCount = 4; break;
+    default: break;
+    }
+
+    for (var i = 0; i < rowCount; ++i) {
+        var row = document.createElement("tr");
+        for (var j = 0; j < colCount; ++j) {
+            var cell = document.createElement("td");
+            if (j == 0)
+                cell.appendChild(document.createTextNode(i == 0 && colCount > 1 ? "[" : "\u00A0"));
+
+            var input = cell.appendChild(document.createElement("input"));
+            input.type = "text";
+            input.maxLength = 5;
+            input.placeholder = "0.0";
+            if (variable.usage === "out")
+                input.readOnly = true;
+            this._fields.push(input);
+            if (j < colCount - 1)
+                cell.appendChild(document.createTextNode(","));
+            else if (colCount > 1 && i == rowCount - 1)
+                cell.appendChild(document.createTextNode("]"))
+            row.appendChild(cell);
+        }
+
+        cellTable.appendChild(row);
+    }
+
+    if (variable.usage === "in" && !!suggestedValue)
+        this.insertValue(suggestedValue);
+
+    if (variable.usage === "in")
+        cellTable.addEventListener("input", this._fieldInputChanged.bind(this));
+}
+
+VariableView.Event = {
+    "InputChanged": "variable-view-input-changed",
+};
+
+VariableView.prototype = {
+    constructor: VariableView,
+    __proto__: GLSL.Object.prototype,
+
+    // Public
+
+    insertValue: function(value)
+    {
+        var dim = 4;
+
+        switch (this.variable.type) {
+        case "float": this._fields[0].value = value; break;
+
+        case "vec2": dim--;
+        case "vec3": dim--;
+        case "vec4":
+            for (var i = 0; i < dim; ++i)
+                this._fields[i].value = value.d[i];
+            break;
+
+        case "vec2": dim--;
+        case "vec3": dim--;
+        case "vec4":
+            for (var i = 0; i < dim; ++i)
+                for (var j = 0; j < dim; ++j)
+                    this._fields[i * dim + j].value = value.d[i][j];
+            break;
+
+        default: break;
+        }
+    },
+
+    extractValue: function()
+    {
+        var values = this._fields.map(function(f) {
+            return (f.value && !f.value.endsWith(".")) ? Number(f.value) : NaN;
+        });
+        if (values.some(function(v) { return isNaN(v); }))
+            return null;
+
+        switch (this.variable.type) {
+            case "float": return values[0];
+            case "vec2": return GLSL.Runtime.Vec2.apply(null, values);
+            case "vec3": return GLSL.Runtime.Vec3.apply(null, values);
+            case "vec4": return GLSL.Runtime.Vec4.apply(null, values);
+            case "mat2": return GLSL.Runtime.Mat2.apply(null, values);
+            case "mat3": return GLSL.Runtime.Mat3.apply(null, values);
+            case "mat4": return GLSL.Runtime.Mat4.apply(null, values);
+        }
+    },
+
+    // Private
+
+    _fieldInputChanged: function(event)
+    {
+        var cell = event.target;
+        cell.classList.remove("valid-input");
+        cell.classList.remove("invalid-input");
+
+        if (cell.value.endsWith(".") || isNaN(Number(cell.value))) {
+            cell.classList.add("invalid-input");
+            return;
+        }
+
+        cell.classList.add("valid-input");
+
+        if (this.extractValue() !== null) // Don't send an event unless all fields complete.
+            this.dispatchEventToListeners(VariableView.Event.InputChanged);
+    }
+};
+
+var VariableListView = function(model, shaderType) {
+    this.model = model;
+    this.shader = shaderType ? this.model.shaderWithType(shaderType) : null;
+
+    this._variableMap = new Map;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("variable-list");
+
+    this.model.addEventListener(DemoModel.Event.ResultChanged, this._resultsChanged, this);
+
+    if (!this.shader)
+        return;
+
+    function createVariable(variable) {
+        var view = new VariableView(variable, this.model.getSuggestedVariableValue(variable));
+        this._variableMap.set(variable, view);
+        this.element.appendChild(view.element);
+
+        if (!variable.readOnly)
+            view.addEventListener(VariableView.Event.InputChanged, this._inputChanged, this);
+    }
+
+    this.shader.uniforms.map(createVariable.bind(this));
+    if (this.shader.type === GLSL.Shader.Type.Vertex)
+        this.shader.attributes.map(createVariable.bind(this));
+
+    this.shader.varyings.map(createVariable.bind(this));
+};
+
+VariableListView.prototype = {
+    constructor: VariableListView,
+    __proto__: GLSL.Object.prototype,
+
+    // Public
+
+    willHide: function()
+    {
+        this.model.removeEventListener(DemoModel.Event.ResultChanged, this._resultsChanged, this);
+    },
+
+    // Private
+
+    _inputChanged: function(event)
+    {
+        var variableView = event.target;
+        this.model.setVariableValue(variableView.variable, variableView.extractValue());
+    },
+
+    _resultsChanged: function()
+    {
+        this._variableMap.forEach(function(value, key, map) {
+            var variable = key, view = value;
+            var storedValue = this.model.env.get(variable.name);
+
+            if (!variable.readOnly && storedValue)
+                view.insertValue(storedValue);
+        }, this);
+    }
+};
